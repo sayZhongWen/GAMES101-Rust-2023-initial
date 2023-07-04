cd8b3d7f775cd4a9184c8e34794efb75f1a0d85a
sayZhongWen <dizzy_d@sjtu.edu.cn>
2023-07-04 11:42:03 +0800

    TASK1: Implement a rotated matrix and a perspective projection matrix

--- John-GAMES101-rs-LAB1/src/utils.rs
+++ John-GAMES101-rs-LAB1/src/utils.rs
@@ -4,25 +4,45 @@ use opencv::core::{Mat, MatTraitConst};
 use opencv::imgproc::{COLOR_RGB2BGR, cvt_color};
 
 pub type V3d = Vector3<f64>;
-
+pub fn degrees_to_radians(a:f64)->f64{
+    a*std::f64::consts::PI/180.0
+}
 pub(crate) fn get_view_matrix(eye_pos: V3d) -> Matrix4<f64> {
     let mut view: Matrix4<f64> = Matrix4::identity();
-    /*  implement your code here  */
-
+    view=view*Matrix4::new(1.0,0.0,0.0,-eye_pos.x,
+    0.0,1.0,0.0,-eye_pos.y,
+    0.0,0.0,1.0,-eye_pos.z,
+    0.0,0.0,0.0,1.0);
     view
 }
 
 pub(crate) fn get_model_matrix(rotation_angle: f64) -> Matrix4<f64> {
-    let mut model: Matrix4<f64> = Matrix4::identity();
-    /*  implement your code here  */
-
+    let a=degrees_to_radians(rotation_angle);
+    let mut model: Matrix4<f64> = Matrix4::new(a.cos(),-a.sin(),0.0,0.0,
+                                               a.sin(),a.cos(),0.0,0.0,
+    0.0,0.0,1.0,0.0,
+    0.0,0.0,0.0,1.0);
     model
 }
 
 pub(crate) fn get_projection_matrix(eye_fov: f64, aspect_ratio: f64, z_near: f64, z_far: f64) -> Matrix4<f64> {
     let mut projection: Matrix4<f64> = Matrix4::identity();
-    /*  implement your code here  */
-
+    let r=z_near*(eye_fov/2.0).tan();
+    let l=-r;
+    let b=r*aspect_ratio;
+    let t=-b;
+    projection= projection*Matrix4::new(2.0/(r-l),0.0,0.0,0.0,
+    0.0,2.0/(t-b),0.0,0.0,
+                                        0.0, 0.0,0.0,2.0/(z_far-z_near),
+                                        0.0, 0.0,0.0,1.0);
+    projection=projection*Matrix4::new(1.0,0.0,0.0,-(r+l)/2.0,
+    0.0,1.0,0.0,-(t+b)/2.0,
+    0.0,0.0,1.0,-(z_far+z_near)/2.0,
+    0.0,0.0,0.0,1.0);
+    projection=projection*Matrix4::new(z_near,0.0,0.0,0.0,
+    0.0,z_near,0.0,0.0,
+    0.0,0.0,z_near+z_far,-z_near*z_far,
+    0.0,0.0,1.0,0.0);
     projection
 }
 
